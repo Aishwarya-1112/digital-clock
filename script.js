dd9d54fc9d0a1684feabdcdeedf912552a0c8d46
Aishwarya1155 <aishwaryamanoharann@gmail.com>
2025-01-30 19:55:46 +0530

    date added

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let ampm = document.getElementById("ampm");
 
 function displayDate() {
   let displayDate = new Date(); //we need date
+  let date = displayDate.getDate();
+  let year = displayDate.getFullYear();
+  let month = displayDate.getMonth();
   let hr = displayDate.getHours();
   let mins = displayDate.getMinutes();
   let sec = displayDate.getSeconds();
@@ -10,6 +13,9 @@ function displayDate() {
     hr = hr - 12;
     ampm.innerHTML = "PM";
   }
+  document.getElementById("date").innerHTML = zero(date);
+  document.getElementById("month").innerHTML = year;
+  document.getElementById("year").innerHTML = month + 1;
   document.getElementById("hours").innerHTML = zero(hr);
   document.getElementById("mins").innerHTML = mins;
   document.getElementById("seconds").innerHTML = zero(sec);
